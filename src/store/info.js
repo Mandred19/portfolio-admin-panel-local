736d84efb2f50bf1e21621db262636387b9d2687
Alexander Alexeev <alekseev@trueconf.ru>
2020-10-22 21:06:00 +0300

    add update main info block

--- src/store/info.js
+++ src/store/info.js
@@ -25,19 +25,66 @@ export default {
     },
     async _updateMainImage({ getters, commit }) {
       try {
-        const image = (await getters.getDB.ref('main-image').once('value')).val() || [];
-        commit('_SET_MAIN_IMAGE', Object.keys(image).map((key) => ({ ...image[key], id: key })));
+        const image = (await getters.getDB.ref('image').once('value')).val() || {};
+        commit('_SET_MAIN_IMAGE', image);
       } catch (e) {
         console.warn(e);
       }
     },
     async _updateBio({ getters, commit }) {
       try {
-        const bio = (await getters.getDB.ref('bio').once('value')).val() || [];
-        commit('_SET_BIO', Object.keys(bio).map((key) => ({ ...bio[key], id: key })));
+        const bio = (await getters.getDB.ref('bio').once('value')).val() || {};
+        commit('_SET_BIO', bio);
       } catch (e) {
         console.warn(e);
       }
+    },
+    async _updateMainInfoList({ dispatch }, dir) {
+      switch (dir) {
+      case 'image': await dispatch('_updateMainImage'); break;
+      case 'bio': await dispatch('_updateBio'); break;
+      default: break;
+      }
+    },
+    async updateMainInfo({ dispatch }, { dir, payload }) {
+      try {
+        await dispatch('_updateMainInfoFile', { dir, payload });
+      } catch (e) {
+        console.warn(e);
+      }
+    },
+    async _saveMainInfoInDB({ getters, dispatch }, { dir, data }) {
+      try {
+        await getters.getDB.ref(dir).set(data);
+
+        await dispatch('_updateMainInfoList', dir);
+      } catch (e) {
+        console.warn(e);
+      }
+    },
+    async _updateMainInfoFile({ getters, dispatch }, { dir, payload }) {
+      const ref = getters.getStorage.ref().child(`${dir}/${payload.file.name}`).put(payload.file);
+      ref.on('state_changed',
+        () => {},
+        (error) => console.warn(error),
+        () => {
+          ref.snapshot.ref.getDownloadURL()
+            .then((fileSrc) => {
+              dispatch('_saveMainInfoInDB', { dir, data: { ...payload, fileSrc } });
+            })
+            .catch((e) => console.warn(e));
+        });
+    },
+    async removeMainInfoFile({ getters }, { dir }) {
+      const ref = getters.getDB.ref(dir);
+      try {
+        const fileName = (await ref.child('fileName').once('value')).val() || '';
+        const fileRef = getters.getStorage.ref().child(fileName);
+        await fileRef.delete();
+      } catch (e) {
+        console.warn(e);
+      }
+      return ref;
     },
   },
 };
--- src/views/AdminInfo.vue
+++ src/views/AdminInfo.vue
@@ -36,33 +36,48 @@
           Main image
         </div>
 
-        <div class="card card-body mb-4">
-          <img class="mb-3" style="width: 300px">
+        <form @submit.prevent="submitHandler('image')" class="card card-body mb-4">
+          <img :src="imageSrcMain" alt="main-image" class="mb-3" style="width: 300px">
 
           <p class="mb-2">Upload new image</p>
 
           <input
+          @change="addFileHandler($event, 'image')"
           type="file"
           :multiple="false"
           accept="image/png, image/jpeg"
           id="add-main-image">
-        </div>
+
+          <br>
+
+          <button type="submit" class="btn btn-primary">Save</button>
+        </form>
 
         <div class="card card-header">
           BIO
         </div>
 
-        <div class="card card-body">
-          <a href="" target="_blank" class="mb-3">View BIO</a>
+        <form @submit.prevent="submitHandler('bio')" class="card card-body">
+          <a :href="urlBio" target="_blank" class="mb-3">View BIO</a>
+
+          <div class="form-group mb-4">
+            <label for="bio-url">BIO URL</label>
+            <input v-model="urlBio" type="url" class="form-control" id="bio-url">
+          </div>
 
           <p class="mb-2">Upload new BIO</p>
 
           <input
+          @change="addFileHandler($event, 'bio')"
           type="file"
           :multiple="false"
           accept="application/pdf"
           id="add-bio">
-        </div>
+
+          <br>
+
+          <button type="submit" class="btn btn-primary">Save</button>
+        </form>
       </div>
     </div>
 </template>
@@ -73,7 +88,7 @@ import { mapActions, mapGetters } from 'vuex';
 export default {
   name: 'AdminInfo',
   methods: {
-    ...mapActions(['removeData']),
+    ...mapActions(['removeData', 'updateMainInfo', 'removeMainInfoFile']),
     async deleteInfo(id) {
       try {
         await this.removeData({ dir: 'info', id });
@@ -81,11 +96,94 @@ export default {
         console.warn(e);
       }
     },
+    addFileHandler(event, type) {
+      if (type === 'image') {
+        // eslint-disable-next-line prefer-destructuring
+        this.imageMain = event.target.files[0];
+        this.imageNameMain = `main-image/${event.target.files[0].name}`;
+      } else if (type === 'bio') {
+        // eslint-disable-next-line prefer-destructuring
+        this.fileBio = event.target.files[0];
+        this.fileNameBio = `bio/${event.target.files[0].name}`;
+      }
+    },
+    async submitHandler(type) {
+      let payload = null;
+      if (type === 'image') {
+        payload = {
+          file: this.imageMain,
+          fileName: this.imageNameMain,
+          fileSrc: this.imageSrcMain,
+        };
+      } else if (type === 'bio') {
+        payload = {
+          file: this.fileBio,
+          fileName: this.fileNameBio,
+          fileSrc: this.fileSrcBio,
+          urlBio: this.urlBio,
+        };
+      }
+      try {
+        if ((this.imageNameMain || this.fileNameBio)
+          && this[type].fileName !== (this.imageNameMain || this.fileNameBio)) {
+          await this.removeMainInfoFile({ dir: type });
+        }
+        await this.updateMainInfo({ dir: type, payload });
+      } catch (e) {
+        console.warn(e);
+      }
+      this.resetForm();
+    },
+    resetForm(type) {
+      if (type === 'image') {
+        this.imageMain = null;
+        this.imageNameMain = null;
+        this.imageSrcMain = null;
+      } else if (type === 'bio') {
+        this.fileBio = null;
+        this.fileNameBio = null;
+        this.fileSrcBio = null;
+        this.urlBio = null;
+      }
+    },
   },
   computed: {
     ...mapGetters({
       adminInfo: 'getAdminInfo',
+      image: 'getMainImage',
+      bio: 'getBio',
     }),
+  },
+  data() {
+    return {
+      imageMain: null,
+      imageNameMain: null,
+      imageSrcMain: null,
+      fileBio: null,
+      fileNameBio: null,
+      fileSrcBio: null,
+      urlBio: null,
+    };
+  },
+  watch: {
+    image: {
+      handler(val) {
+        if (val) {
+          const { fileSrc } = this.image;
+          this.imageSrcMain = fileSrc;
+        }
+      },
+      immediate: true,
+    },
+    bio: {
+      handler(val) {
+        if (val) {
+          const { urlBio } = this.bio;
+          this.urlBio = urlBio;
+        }
+      },
+      immediate: true,
+    },
   },
 };
 </script>
